50874c5a6f34ad89b7229b440e3dd84325907a0d
Charlie Crawford <ccrawford@pagerduty.com>
2021-04-28 12:13:38 -0400

    Post thing

--- src/PagerDutyThing.js
+++ src/PagerDutyThing.js
@@ -3,19 +3,38 @@ import {api} from '@pagerduty/pdjs';
 
 const pd = api({token: 'someToken1234567890'});
 
-const handleClick = () => {
-    console.log("Clicked")
+const handleGetClick = () => {
+    console.log("GET Clicked")
 
     pd.get('/incidents')
     .then(({data, resource, next}) => console.log(data, resource, next))
     .catch(console.error);
 }
 
+const handlePostClick = () => {
+    console.log("POST Clicked")
+    const body = { 
+        incident: {
+            title: "CORS may not be working properly"
+        } 
+    }
+
+    pd.post('/incidents', body)
+    .then(({data, resource, next}) => console.log(data, resource, next))
+    .catch(console.error);
+}
+
 function PagerDutyThing() {
     return (
-        <button onClick={handleClick}>
-            Do PagerDuty Thing
-        </button>
+        <div>
+            <button onClick={handleGetClick}>
+                Get PagerDuty Thing
+            </button>
+            <button onClick={handlePostClick}>
+                Create PagerDuty Thing
+            </button>
+        </div>
+
     );
 }
 
