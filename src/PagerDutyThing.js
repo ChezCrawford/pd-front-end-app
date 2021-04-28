ef22831bf6d653361c8949fef9d9e5aa2c119994
Charlie Crawford <ccrawford@pagerduty.com>
2021-04-28 16:32:51 -0400

    Additional scenarios.

--- src/PagerDutyThing.js
+++ src/PagerDutyThing.js
@@ -1,7 +1,7 @@
 
 import {api} from '@pagerduty/pdjs';
 
-const pd = api({token: 'someToken1234567890'});
+const pd = api({token: 'y_NbAkKc66ryYTWUXYEu'});
 
 const handleGetClick = () => {
     console.log("GET Clicked")
@@ -13,13 +13,39 @@ const handleGetClick = () => {
 
 const handlePostClick = () => {
     console.log("POST Clicked")
-    const body = { 
+    const body = {
         incident: {
             title: "CORS may not be working properly"
-        } 
+        }
     }
 
     pd.post('/incidents', body)
+    .then(({data, resource, next}) => console.log(data, resource, next))
+    .catch(console.error);
+}
+
+const handlePutClick = () => {
+    console.log("PUT Clicked")
+    const body = {
+        incident: {
+            title: "CORS may not be working properly"
+        }
+    }
+
+    pd.put('/incidents/PRVZI4Q', body)
+    .then(({data, resource, next}) => console.log(data, resource, next))
+    .catch(console.error);
+}
+
+const handleEarlyAccessClick = () => {
+    console.log("GET Clicked")
+
+    fetch('https://api.pagerduty.com/status_dashboards', {
+        headers: {
+            'Accept': 'application/vnd.pagerduty+json;version=2',
+            'X-Early-Access': 'status-dashboards'
+        }
+    })
     .then(({data, resource, next}) => console.log(data, resource, next))
     .catch(console.error);
 }
@@ -32,6 +58,12 @@ function PagerDutyThing() {
             </button>
             <button onClick={handlePostClick}>
                 Create PagerDuty Thing
+            </button>
+            <button onClick={handlePutClick}>
+                Update PagerDuty Thing
+            </button>
+            <button onClick={handleEarlyAccessClick}>
+                Early PagerDuty Thing
             </button>
         </div>
 
